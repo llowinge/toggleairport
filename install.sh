7089a5816c2d1c80c96b2a877891dfa28c6a12ca
Lukas Lowinger <llowinge@redhat.com>
2021-02-10 17:05:52 +0100

    [fix] Detect Ethernet connection via plain USB connection because when on VPN, no changes in network files are made, so script is not triggered with VPN

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 #git@github.com:paulbhart/toggleairport.git
 #originally from https://gist.github.com/albertbori/1798d88a93175b9da00b
 
+(TMP_FOLDER="/tmp/$(date +%Y-%m-%d-%T)" && mkdir -p $TMP_FOLDER && cd $TMP_FOLDER && git clone git@github.com:snosrap/xpc_set_event_stream_handler.git && cd xpc_set_event_stream_handler/xpc_set_event_stream_handler && gcc -framework Foundation -o xpc_set_event_stream_handler main.m && cp xpc_set_event_stream_handler /usr/local/bin/)
+
 sudo cp ./toggleAirport.sh /Library/Scripts/
 sudo chmod 755 /Library/Scripts/toggleAirport.sh
 
